7bdb534b340b90cda617269743423a4bf063a95c
ajiBal <121741620+ajiBal@users.noreply.github.com>
2024-02-14 09:00:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt update
 apt install fluxbox xterm font-terminus x11vnc wget xvfb
 echo installing ngrok
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar xf ngrok*
+tar xf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok config add-authtoken 2HOfzFybejy2wCyxIcOPMGLP9KP_4Yf8Mwg21ps9upjhi6z7J
 echo starting ngrok in background
 nohup ./ngrok tcp 23 --region ap &
