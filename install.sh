364d142d43303e341e1fb791e29e1f63f833d35e
ajiBal <121741620+ajiBal@users.noreply.github.com>
2024-02-14 08:06:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 echo installing script starting...
 echo script by idkwannawhatname
-apk update
-apk add fluxbox xterm font-terminus x11vnc wget xvfb
+apt update
+apt install fluxbox xterm font-terminus x11vnc wget xvfb
 echo installing ngrok
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar xf ngrok*
-./ngrok config add-authtoken 240SGBnuVksey8FUdanesdcoLDr_ZTSFjjkZaTd7uL4MKB1R
+./ngrok config add-authtoken 2HOfzFybejy2wCyxIcOPMGLP9KP_4Yf8Mwg21ps9upjhi6z7J
 echo starting ngrok in background
-nohup ./ngrok tcp 23 --regiom ap &
+nohup ./ngrok tcp 23 --region ap &
 echo script done install all packages needed
 echo now u need manually run start script to prevent error
